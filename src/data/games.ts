864bc1c3e25628cd5341509b91161b2a259ea2a4
Tom Shea <tom@shea.at>
2019-12-01 13:00:32 -0800

    use promises for db access

--- src/data/games.ts
+++ src/data/games.ts
@@ -3,22 +3,19 @@ import sql from "mssql";
 
 const sqlInfo: sql.config = config.get("SQL_INFO");
 const pool = new sql.ConnectionPool(sqlInfo);
+const connectionPromise = pool.connect();
 
-export const insertGameToDB = async (id: string) => {
-    const connection = await pool.connect();
+export const insertGameToDB = async (id: string): Promise<boolean> => {
+    const connection = await connectionPromise;
     const ps = new sql.PreparedStatement(connection);
     ps.input("id", sql.UniqueIdentifier);
-    ps.prepare(`INSERT INTO Games(id) VALUES (@id)`, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        ps.execute({ id }, (err, result) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            ps.unprepare();
-        });
-    });
+    try {
+        await ps.prepare(`INSERT INTO Games(id) VALUES (@id)`);
+        await ps.execute({ id });
+        await ps.unprepare();
+        return true;
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
 };
