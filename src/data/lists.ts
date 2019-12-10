1ee4ea748dd99a5e98007ca90c962fa052bc9cc7
Tom Shea <tom@shea.at>
2019-12-09 20:13:31 -0800

    getLists initial implementation

--- src/data/lists.ts
+++ src/data/lists.ts
@@ -1,19 +1,56 @@
 import config from "config";
 import sql from "mssql";
+import { ListItem, ListItemQuery } from "../routes/api/lists";
 
 const sqlInfo: sql.config = config.get("SQL_INFO");
 const pool = new sql.ConnectionPool(sqlInfo);
 const connectionPromise = pool.connect();
 
-export const getListFromDB = async (id: string): Promise<boolean> => {
+export const getListFromDB = async (id: string): Promise<ListItem[]> => {
     const connection = await connectionPromise;
     const ps = new sql.PreparedStatement(connection);
-    ps.input("id", sql.UniqueIdentifier);
+    console.log(id);
+    ps.input("id", sql.VarChar);
+    let listItems: ListItem[] = [];
     try {
-        await ps.prepare(`SELECT gameId FROM ListItems LI
-            INNER JOIN List L ON L.id = LI.listId
-            WHERE userId = (@id)`);
+        await ps.prepare(`SELECT gameId, played FROM ListItems LI
+            INNER JOIN Lists L ON L.id = LI.listId
+            WHERE userId = @id`);
         let result = await ps.execute({ id });
+        listItems = [];
+        console.log("result.recordset");
+        console.dir(result.recordset);
+        result.recordset.map((x) => {
+            listItems.push({ gameId: x.gameId, played: x.played });
+        });
+        console.log("listItems");
+        console.dir(listItems);
+        await ps.unprepare();
+        return listItems;
+    } catch (e) {
+        console.error(e);
+        return listItems;
+    }
+};
+
+export const setListItemPlayedInDB = async (listItem: ListItemQuery): Promise<boolean> => {
+    const connection = await connectionPromise;
+    const ps = new sql.PreparedStatement(connection);
+    let { gameExtId, userId, played } = listItem;
+    ps.input("gameId", sql.UniqueIdentifier);
+    ps.input("userId", sql.VarChar);
+    ps.input("played", sql.Bit);
+    try {
+        await ps.prepare(`
+            UPDATE ListItems
+            SET played = @played
+            FROM ListItems LI
+            INNER JOIN Lists L ON L.id = LI.listId
+            INNER JOIN Games G ON G.id = LI.gameId
+            WHERE L.userID = @userId
+            AND G.externalId = @gameExtId
+        `);
+        let result = await ps.execute({ gameExtId, userId, played });
         console.log(result);
         await ps.unprepare();
         return true;
--- src/routes/api/lists.ts
+++ src/routes/api/lists.ts
@@ -1,13 +1,33 @@
 import { Middleware, Next, ParameterizedContext } from "koa";
-import { getListFromDB } from "../../data/lists";
-import { GBGame } from "../../lib/giantbomb_model";
+import { getListFromDB, setListItemPlayedInDB } from "../../data/lists";
 
-export interface GamesApiResponse {
-    games: GBGame[];
+export interface ListItem {
+    gameId: string;
+    listId?: string;
+    played: boolean;
+}
+
+export interface ListItemQuery {
+    gameExtId: string;
+    userId?: string;
+    played: boolean;
 }
 
 export const getList: Middleware = async (ctx: ParameterizedContext, next: Next) => {
     const list = await getListFromDB(ctx.state.user.id);
+    if (list.length != 0) {
+        ctx.body = list;
+    } else {
+        ctx.status = 500;
+        ctx.body = "DB Error.";
+        return;
+    }
+};
+
+export const setListItemPlayed: Middleware = async (ctx: ParameterizedContext, next: Next) => {
+    let listItem: ListItemQuery = ctx.request.body;
+    listItem.userId = ctx.state.user.id;
+    const list = await setListItemPlayedInDB(ctx.state.user.id);
     if (list != undefined) {
         ctx.body = list;
     } else {
