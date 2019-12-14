3e8915677ad0185a74445a866486dffbd8d62b85
Tom Shea <thristhart@gmail.com>
2019-12-14 15:34:45 -0800

    Improve getList performance by caching default games (#11)

--- src/routes/api/lists.ts
+++ src/routes/api/lists.ts
@@ -1,10 +1,11 @@
 import { Middleware, Next, ParameterizedContext } from "koa";
 import { getListFromDB, setListItemPlayedInDB } from "../../data/lists";
+import { get } from "../../lib/cache";
 import { getGameDetailFromGiantbomb } from "../../lib/giantbomb";
 import { GBGame } from "../../lib/giantbomb_model";
 
 export interface ListItem {
-    gameId: string;
+    gameId: number;
     listId?: string;
     played: boolean;
     gameDetails?: GBGame;
@@ -15,12 +16,24 @@ export interface ListItemQuery {
     userId?: string;
     played: boolean;
 }
+const getDefaultList = get("default_list")
+    .then((items) => JSON.parse(items))
+    .then((items: ListItem[]) => {
+        const map = new Map<number, GBGame>();
+        items.forEach((item) => {
+            map.set(item.gameId, item.gameDetails!);
+        });
+        return map;
+    });
 
 export const getList: Middleware = async (ctx: ParameterizedContext, next: Next) => {
     const list = await getListFromDB(ctx.state.user.id);
+    const defaultHydratedList = await getDefaultList;
     const fetches = list.map(async (item) => {
-        item.gameDetails = await getGameDetailFromGiantbomb("3030-" + item.gameId);
+        item.gameDetails =
+            defaultHydratedList.get(item.gameId) || (await getGameDetailFromGiantbomb("3030-" + item.gameId));
     });
+
     await Promise.all(fetches);
     if (list.length != 0) {
         ctx.body = list;
